7306120f2570b8fbc6246f9fa07610344e984941
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2021-08-26 09:08:59 -0600

    Add code to meals.js, remove skips on it blocks of meal-test.js file

--- src/meal.js
+++ src/meal.js
@@ -11,16 +11,32 @@ function createMenuItem(name, price, type) {
  return menuItem
 }
 
-function addIngredients(ingredients) {
-  var newIngredient = ingredients.unshift(ingredients)
+function addIngredients(foodItem, ingredients) {
+  for (var i = 0; i < ingredients.length; i++) {
+    if (foodItem === ingredients[i]) {
+      return;
+    }
+  }
+  ingredients.push(foodItem)
 }
 
+function formatPrice(price) {
+  return `$${price}`;
+}
+
+function decreasePrice(price) {
+  return price - (price * 0.1)
+}
+
+function createRecipe() {
+
+}
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
   addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
