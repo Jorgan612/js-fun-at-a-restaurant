2a453b50a575d642aa8aa408677204858c9488d7
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2021-08-25 20:04:15 -0600

    Add code to meal.js file and remove skip from test it blocks

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,26 @@
-function nameMenuItem() {
+function nameMenuItem(foodName) {
+  return `Delicious ${foodName}`
+}
 
+function createMenuItem(name, price, type) {
+ var menuItem = {
+   name: name,
+   price: price,
+   type: type,
+ }
+ return menuItem
+}
+
+function addIngredients(ingredients) {
+  var newIngredient = ingredients.unshift(ingredients)
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
