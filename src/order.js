827fb9bf9bb65aa1779ce8482e37513c6d9b2fa2
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2021-08-28 11:10:44 -0600

    Add code to order.js file and unskip it blocks in order-test.js file

--- src/order.js
+++ src/order.js
@@ -1,7 +1,38 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    return deliveryOrders.push(order);
+  }
+}
+
+function refundOrder(orderNum, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (orderNum === deliveryOrders[i].orderNumber) {
+      return deliveryOrders.splice(i,1);
+    }
+  }
+}
+
+function listItems(deliveryOrders) {
+  var orderItems = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    orderItems.push(deliveryOrders[i].item);
+  }
+  return orderItems.join(", ");
+}
+
+function searchOrder(deliveryOrders, foodItem) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === foodItem) {
+      return true;
+      }
+    }
+    return false;
+  }
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
