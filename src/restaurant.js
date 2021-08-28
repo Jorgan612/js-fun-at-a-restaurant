996b7cd473953cd25769ef36e4386ac396db330d
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2021-08-28 16:36:36 -0600

    Add code to restaurant.js file and unskip it blocks in restaurant-test.js file

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,34 @@
+function createRestaurant(name, menus) {
+ var newRestaurant = {
+   name: name,
+   menus: {
+     breakfast: [],
+     lunch: [],
+     dinner: []
+   }
+ }
+  return newRestaurant;
+}
 
+function addMenuItem(newRestaurant, newMenuItem) {
+  // for (var i = 0; i < newRestaurant.menus[newMenuItem.type.length]; i++) {
+
+    // console.log(newRestaurant.menus[newMenuItem.type.length])
+    // console.log(newRestaurant.menus[newMenuItem[i]]);
+    console.log('1', newMenuItem.name);
+    if (newMenuItem.name === newRestaurant.menus[newMenuItem.type]) {
+      console.log('2', newRestaurant.menus[newMenuItem.type]);
+
+      // console.log(newRestaurant.menus[newMenuItem[i]]);
+      return 'test';
+    } else {
+      return newRestaurant.menus[newMenuItem.type].push(newMenuItem);
+    }
+  // }
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant,
+  addMenuItem,
   // removeMenuItem
 }
